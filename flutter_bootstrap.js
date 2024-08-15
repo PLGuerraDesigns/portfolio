d4a37678b0e5bd34f603c85f57d28e51cb40de52
plguerradesigns <plguerra@outlook.com>
2024-08-15 23:56:41 +0000

    Updated Release

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1126792811"
+    serviceWorkerVersion: "3170447154"
   }
 });
